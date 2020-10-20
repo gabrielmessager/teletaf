abc4b91612eb9ea0cf67ca487748147edfc6e199
Gabriel Messager <gabriel@frame.io>
2020-10-20 11:55:38 +0200

    remove console.log

--- hooks/useWindowWidth/useWindowWidth.js
+++ hooks/useWindowWidth/useWindowWidth.js
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 
 export function useWindowWidth(initialWidth = 0) {
-  console.log("window", window);
   const [width, setWidth] = useState({
     windowWidth: process.browser ? window.innerWidth : initialWidth,
     // windowWidth: process.browser ? window?.screen?.width : initialWidth,
