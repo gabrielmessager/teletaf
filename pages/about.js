05df740c698fcc7eaf38721dad67db1ec37f3919
Gabriel Messager <gabriel@frame.io>
2020-10-27 12:13:12 +0100

    fix about page import

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import { About as AboutPage } from "../components/pages/About";
+import { About as AboutPage } from "../components/pages/about";
 
 export default function About() {
   return <AboutPage />;
