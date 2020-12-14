640ad3f8766f32f9a9554f14b39294c9a66b5584
Gabriel Messager <gabriel@frame.io>
2020-12-14 22:13:10 +0100

    attempt to fix path to image in metatag

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ export default function App({ Component, pageProps, router }) {
           rel="stylesheet"
         />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:image" content="../public/preview.jpg" />
+        <meta property="og:image" content="/preview.jpg" />
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
