529ff4cca3ebd32b9d4d14db9c50dd84b410a86d
Gabriel Messager <gabriel@frame.io>
2021-01-26 11:06:21 +0100

    add react-hook-form

--- components/JobPostForm/JobPostForm.js
+++ components/JobPostForm/JobPostForm.js
@@ -1,51 +1,51 @@
-import React, { useState } from "react"
-import { JobPostTitle } from "./components/JobPostTitle"
+import React, { useState } from 'react';
+// import { JobPostTitle } from './components/JobPostTitle';
 
-import { JobPostDesign } from "./components/JobPostDesign"
-import { JobPostDescription } from "./components/JobPostDescription"
-import { Container, Flex } from "./JobPostForm.styles"
+// import { JobPostDesign } from './components/JobPostDesign';
+import { JobPostDescription } from './components/JobPostDescription';
+import { Container, Flex } from './JobPostForm.styles';
 
 export function JobPostForm({ children }) {
   const [jobPostTitle, setJobPostTitle] = useState({
-    title: "",
-    companyName: "",
-  })
+    title: '',
+    companyName: '',
+  });
   const [jobPostDesign, setJobPostDesign] = useState({
     showLogo: false,
     isRecurring: false,
     shouldHiglight: false,
     color: null,
-  })
+  });
   const [jobDescription, setJobDescription] = useState({
     email: null,
     password: null,
-  })
+  });
 
-  console.log("showLogo", jobPostDesign.showLogo)
-  console.log("isRecurring", jobPostDesign.isRecurring)
-  console.log("shouldHiglight", jobPostDesign.shouldHiglight)
-  console.log("color", jobPostDesign.color)
+  console.log('showLogo', jobPostDesign.showLogo);
+  console.log('isRecurring', jobPostDesign.isRecurring);
+  console.log('shouldHiglight', jobPostDesign.shouldHiglight);
+  console.log('color', jobPostDesign.color);
 
   return (
     <Flex>
       <Container>
-        <JobPostTitle setJobPostTitle={setJobPostTitle} />
-        <JobPostDesign setJobPostDesign={setJobPostDesign} />
+        {/* <JobPostTitle setJobPostTitle={setJobPostTitle} /> */}
+        {/* <JobPostDesign setJobPostDesign={setJobPostDesign} /> */}
         <JobPostDescription setJobDescription={setJobDescription} />
       </Container>
       <Container>
         <div
           style={{
-            width: "100%",
-            height: "100%",
-            border: "solid 1px #ddd",
-            "border-radius": "8px",
-            "margin-bottom": "32px",
+            width: '100%',
+            height: '100%',
+            border: 'solid 1px #ddd',
+            'border-radius': '8px',
+            'margin-bottom': '32px',
           }}
         >
           <span>testimonial</span>
         </div>
       </Container>
     </Flex>
-  )
+  );
 }
--- components/JobPostForm/components/JobPostDescription/JobPostDescription.js
+++ components/JobPostForm/components/JobPostDescription/JobPostDescription.js
@@ -1,61 +1,33 @@
 import React from 'react';
-import * as Yup from 'yup';
-import { Formik, Form, Field, ErrorMessage, withFormik } from 'formik';
+// import * as Yup from 'yup';
+// import { Formik, Form, Field, ErrorMessage, withFormik } from 'formik';
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-
-const MUST_BE_AN_EMAIL_ADDRESS = 'Must be an email address';
 
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
 `;
 
-function InnerForm({
-  onClose,
-  handleSubmit,
-  handleChange,
-  handleBlur,
-  isValid,
-  touched,
-  errors,
-}) {
+export function JobPostDescription() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log('errors', errors);
+  console.log(watch('email'));
+  console.log(watch('password'));
+
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        touched={touched}
-        errors={errors}
-      />
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
+      <input type="email" name="email" ref={register({ required: true })} />
       <input
         type="password"
         name="password"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        touched={touched}
-        errors={errors}
+        ref={register({ required: true })}
       />
+      {errors.password && <span>Password is required</span>}
+      {errors.email && <span>Email is required</span>}
       <button type="submit">Submit</button>
     </StyledForm>
   );
 }
-
-const formikOptions = {
-  handleSubmit: (values, { props }) => {
-    props.setJobDescription({ email: values.email, password: values.password });
-  },
-  validateOnChange: true,
-  validationSchema: Yup.object().shape({
-    email: Yup.string()
-      .email(MUST_BE_AN_EMAIL_ADDRESS)
-      .required('Email should not be blank'),
-
-    password: Yup.string()
-      .required('No password provided.')
-      .min(8, 'Password is too short - should be 8 chars minimum.'),
-  }),
-};
-
-export const JobPostDescription = withFormik(formikOptions)(InnerForm);
